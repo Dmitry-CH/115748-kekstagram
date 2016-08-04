29ba52bdf41bd11602c5ead47b4c71554b05e233
Dmitry-CH <dmit.checkanov@yandex.ru>
2016-08-04 22:00:40 +0300

    Доработка функции анализа картинок

--- build/js/check.js
+++ build/js/check.js
@@ -2,61 +2,59 @@
 
 function getMessage(a, b) {
 
-  var resultat;
+  var result;
 
   if (typeof a === 'boolean' && b !== undefined) {
-    resultat = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    result = 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
   }else {
-    resultat = 'Переданное GIF-изображение не анимировано';
+    result = 'Переданное GIF-изображение не анимировано';
   };
 
   if (typeof a === 'number') {
-    resultat = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+    result = 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
   };
 
   if (Array.isArray(a)) {
     var amountOfRedPoints = sumArray(a);
 
-    resultat = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
+    result = 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   };
 
   if (Array.isArray(a) && Array.isArray(b)) {
     var artifactsSquare = sumArray(a, b);
 
-    resultat = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+    result = 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
   };
 
-  return resultat;
-};
+  return result;
 
-// Вспомогательная функция
+  //
+  // Вспомогательная функция
 
-function sumArray(arr1, arr2) {
+  function sumArray(arr1, arr2) {
 
-  var arrTrue1 = Array.isArray(arr1),
-      arrTrue2 = Array.isArray(arr2);
+    var arrTrue1 = Array.isArray(arr1),
+        arrTrue2 = Array.isArray(arr2);
 
-  var sumArr1  = 0,
-      sumArr2  = 0,
-      allSum;
+    var sumArr1  = 0,
+        sumArr2  = 0,
+        allSum;
 
-  if (!arrTrue1 && !arrTrue2) {
-    alert('Не было передано ни одного массива!');
+    if (arrTrue1) {
+      for (var i = 0; i < arr1.length; i++) {
+        sumArr1 += arr1[i];
+      };
 
-    return;
-  };
-
-  if (arrTrue1) {
-    for (var i = 0; i < arr1.length; i++) {
-      sumArr1 += arr1[i];
+      if (arrTrue2) {
+        for (i = 0; i < arr2.length; i++) {
+          sumArr2 += arr2[i];
+        };
+      };
+    }else {
+      alert('Не было передано ни одного массива!');
+      return;
     };
-  };
 
-  if (arrTrue2) {
-    for (i = 0; i < arr2.length; i++) {
-      sumArr2 += arr2[i];
-    };
+    return allSum = sumArr1 + sumArr2;
   };
-
-  return allSum = sumArr1 + sumArr2;
 };
