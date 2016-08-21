841ef34856cdaf84bf04cf6ea05f4b86356a5a5a
Dmitry-CH <dmit.checkanov@yandex.ru>
2016-08-21 17:15:22 +0300

    Добавлен шаблонизатор для изображений

--- src/pictures.js
+++ src/pictures.js
@@ -5,10 +5,71 @@
   var callbackName = 'jsonpCallback';
   var url = 'http://localhost:1506/api/pictures?callback=' + callbackName;
 
-  var savePictures = function(data) {
+  // Получаю элемент контейнер, куда буду помещать новые элементы.
+  var picturesContainer = document.querySelector('.pictures');
+  // Получаю элемент шаблон 'template'.
+  var templateElement = document.querySelector('#picture-template');
+  // Переменная куда помещаю нужный элемент из шаблона.
+  var elementToClone;
+
+  // Условие проверки.
+  if ('content' in templateElement) {
+    elementToClone = templateElement.content.querySelector('.picture');
+  }else {
+    elementToClone = templateElement.querySelector('.picture');
+  }
+
+  /**
+   *
+   */
+
+  function getPictureElement(data, container) {
+    var element = elementToClone.cloneNode(true);
+    var pictureStats = element.querySelector('.picture-stats');
+
+    var newImg = new Image(182, 182);
+
+    newImg.onload = function() {
+      element.insertBefore(newImg, pictureStats);
+
+      var allImg = element.querySelectorAll('img');
+      element.removeChild(allImg[0]);
+    };
+
+    newImg.onerror = function() {
+      element.classList.add('picture-load-failure');
+    };
+
+    newImg.src = data.url;
+
+    element.querySelector('.picture-comments').textContent = data.comments;
+    element.querySelector('.picture-likes').textContent = data.likes;
+    container.appendChild(element);
+
+    return element;
+  }
+
+  /**
+   *
+   */
+
+  var renderPictures = function(data) {
+    var filtersElement = document.querySelector('.filters');
+    // Прячем блок filters, добавляя класс 'hidden'.
+    filtersElement.classList.add('hidden');
+
+    // Сохраняем полученный список изображений в переменную.
     pictures = data;
-    console.log(pictures);
+
+    // Перебираем список изображений и применяем шаблон.
+    pictures.forEach(function(img) {
+      getPictureElement(img, picturesContainer);
+    });
   };
+
+  /**
+   *
+   */
 
   function getJSONP(src, callback) {
     window[callbackName] = callback;
@@ -19,5 +80,6 @@
     document.body.appendChild(scriptEl);
   }
 
-  getJSONP(url, savePictures);
+  // Выполняем JSONP запрос на сервер.
+  getJSONP(url, renderPictures);
 })();
