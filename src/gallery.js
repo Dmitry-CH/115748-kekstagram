afe1548742fb7d1561fc3e1b3c8c24600eff54ad
Dmitry-CH <dmit.checkanov@yandex.ru>
2016-08-29 15:29:49 +0300

    Модуль gallery, события объекта перенесены в prototype

--- src/gallery.js
+++ src/gallery.js
@@ -1,10 +1,9 @@
 'use strict';
 
-// Конструктор объектов 'Gallery'.
+/**
+  * Конструктор объектов 'Gallery'.
+  */
 var Gallery = function() {
-  // Сохраняем текущий контекст.
-  var self = this;
-
   this.pictures = [];
   this.activePicture = 0;
 
@@ -14,6 +13,21 @@ var Gallery = function() {
   this.galleryContainer = document.querySelector('.gallery-overlay');
   this.galleryClose = document.querySelector('.gallery-overlay-close');
   this.galleryImage = document.querySelector('.gallery-overlay-image');
+};
+
+/**
+  * Общие методы всех объектов 'Gallery'.
+  */
+Gallery.prototype.setPictures = function(arrPictures) {
+  this.pictures = arrPictures;
+
+  // Записываем в 'picturesLength' длину полученного массива.
+  this.picturesLength = arrPictures.length;
+};
+
+Gallery.prototype.show = function(number) {
+  // Сохраняем в переменную ссылку на текущий объект.
+  var self = this;
 
   // Добовляем обработчики событий.
   // Клик по 'Х', закрытие 'Gallery'.
@@ -31,19 +45,7 @@ var Gallery = function() {
       self.setActivePicture(0);
     }
   };
-};
 
-/**
-  * Общие методы всех объектов 'Gallery'
-  */
-Gallery.prototype.setPictures = function(arrPictures) {
-  this.pictures = arrPictures;
-
-  // Записываем в 'picturesLength' длину полученного массива.
-  this.picturesLength = arrPictures.length;
-};
-
-Gallery.prototype.show = function(number) {
   this.galleryContainer.classList.remove('invisible');
   this.setActivePicture(number);
 };
@@ -52,16 +54,16 @@ Gallery.prototype.hide = function() {
   this.galleryContainer.classList.add('invisible');
 
   // Удаляем обработчики событий.
-  //this.galleryClose.onclick = null;
-
-  //this.galleryImage.onclick = null;
+  this.galleryClose.onclick = null;
+  this.galleryImage.onclick = null;
 };
 
 Gallery.prototype.setActivePicture = function(number) {
   this.activePicture = +number;
-  console.log(this.activePicture);
+
   this.galleryImage.src = this.pictures[this.activePicture].url;
 
+  // Записывает в DOM-элементы количество лайков и комментариев для показанной фотографии.
   this.galleryContainer.querySelector('.likes-count').innerHTML = this.pictures[this.activePicture].likes;
   this.galleryContainer.querySelector('.comments-count').innerHTML = this.pictures[this.activePicture].comments;
 };
